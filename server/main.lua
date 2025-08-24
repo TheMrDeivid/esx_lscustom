e80e45517b41e5de5032196f9cb7e373ce3aa958
David Carneiro <mrdeividcg@gmail.com>
2025-08-24 04:13:17 +0100

    Adding Repair Menu
    Adding a repair menu to detect if the car is damage before opening the main menu

--- server/main.lua
+++ server/main.lua
@@ -77,3 +77,14 @@ ESX.RegisterServerCallback('esx_lscustom:getVehiclesPrices', function(source, cb
 		cb(Vehicles)
 	end
 end)
+
+ESX.RegisterServerCallback('esx_lscustom:payForRepair', function(source, cb, price)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if xPlayer.getMoney() >= price then
+        xPlayer.removeMoney(price)
+        cb(true)
+    else
+        cb(false)
+    end
+end)
